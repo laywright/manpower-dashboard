b6a1388daa0c7eec9722bc079c68a33a94479a30
Alex Muiruri <114861950+laywright@users.noreply.github.com>
2025-07-24 20:05:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ if uploaded_file is not None:
     avg_manhours = total_df['Manhours'].mean()
 
     # Tabs
-    tab1, tab2, tab3, tab4 = st.tabs(["Summary", "Process time analysis", "Human resource allocation gaps", "Extra Tab"])
+    tab1, tab2, tab3, tab4 = st.tabs(["Summary", "Process time analysis", "Human resource allocation gaps"])
 
     # -------------------- TAB 1 --------------------
     with tab1:
